22f510e3cb5bb21ffb07f046f98f0de812d76cad
abdullahcangul <abdullahcangul@gmail.com>
2018-08-27 14:19:03 +0300

    validation finished

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,22 @@ const movieSchema=new Schema({
     director_id:Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true
+        required:true,
+        required:  [true, '`{PATH}` alanı zorunludur.'],
+			maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır '],
+			minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
     },
-    category:String,
+    category: {
+    	type: String,
+			maxlength: 30,
+			minlength: 1,
+		},
     country:String,
-    year:Number,
-    
+    year: {
+    	type: Number,
+			max: 2040,
+			min: 1900
+		},
     imdb_score:Number,
     createdAt:{
         type:Date,
