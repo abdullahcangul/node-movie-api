fa584304c505cbcecbf2c5a59e9dd52f6ceaf697
abdullahcangul <abdullahcangul@gmail.com>
2018-08-28 15:10:13 +0300

    JTW endpoind

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const director=require("./routes/director")
 const app = express();
 //db connection
 const db=require("./helper/db.js")()
+//Config
+const config=require("./config")//simdi eklendi
+app.set("api_secret_key",config.api_secret_key)
+// Middleware
+const verifyToken = require('./middleware/verify-token');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/app/movies', movie);//burası degisti
 app.use("/app/directors",director)
+app.use("/api",verifyToken)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
